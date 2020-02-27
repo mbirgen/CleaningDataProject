f5bd3a954a04f109fb57a2505817f30a1832f535
Mariah Birgen <mariah.birgen@wartburg.edu>
2020-02-27 13:37:19 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -29,8 +29,13 @@ activity_train <- select(activity_train, activity)
 train <-cbind(subject_train,activity_train, train)
 test <-cbind(subject_test,activity_test, test)
 
+## Clean unnecessary data files
+rm(activities); rm(activity_test) ; rm(activity_train) ; rm(features);
+rm(label_test); rm(label_train) ; rm(subject_test); rm(subject_train);
 
-## Code assumes data has been downloaded, merged with subject and activity, 
+
+
+## From here, code assumes data has been downloaded, merged with subject and activity, 
 ## and saved into two files, test and train.
 
 ## First merge tables test and train.
@@ -39,6 +44,7 @@ my_df <-rbind.data.frame(train, test)
 col_names <- grep("mean\\(|std\\(", names(my_df))
 ## Make sure we don't use information on the subject
 col_names <- c(1,2, col_names)
+
 ## Select appropriate columns for the new data frame.
 my_df <- my_df[,col_names]
 ## Make all names lower case and clean extra symbols
@@ -46,12 +52,14 @@ names(my_df) <- tolower(names(my_df))
 names(my_df) <-gsub("\\(\\)", "", names(my_df))
 ## Clarify names by expanding abreviations
 names(my_df) <-gsub("^t", "time", names(my_df))
-names(my_df) <-gsub("^f", "force", names(my_df))
+names(my_df) <-gsub("^f", "fastfouriertransform", names(my_df))
 names(my_df) <-gsub("std", "standarddeviation", names(my_df))
 names(my_df) <-gsub("acc", "acceleration", names(my_df))
 names(my_df) <-gsub("mag", "magnitude", names(my_df))
 ## name dataframe something useful
 trainplustrialfulldata <- my_df
+## Clean unused data
+rm(my_df) ; rm(my_df2) ; rm(col_names) ; rm(test) ; rm(train);
 View(trainplustrialfulldata)
 
 library(dplyr)
@@ -60,5 +68,7 @@ my_df2 <-group_by(my_df, subject, activity)
 ## Create new dataframe that calculates the mean of grouped data
 my_df_sum <-summarize_all(my_df2, mean)
 trainplustrialmeans <- my_df_sum
+## Clean unused data
+rm(my_df_sum)
 View(trainplustrialmeans)
 
