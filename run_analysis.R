7a16fd5c0cca335fffcae4cfa7038256d6047c84
Mariah Birgen <mariah.birgen@wartburg.edu>
2020-02-27 10:35:04 -0600

    Final edits,  Decided to add the read.table commands to the main program.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,35 @@
+## Load data from computer into a variety of tables.
+subject_train <-read.table("./data/train/subject_train.txt")
+subject_test <-read.table("./data/test/subject_test.txt")
+train  <-read.table("./data/train/X_train.txt")
+test <- read.table("./data/test/X_test.txt")
+label_train  <-read.table("./data/train/y_train.txt")
+label_test <- read.table("./data/test/y_test.txt")
+
+features <- read.table("./data/features.txt")
+activities <- read.table("./data/activity_labels.txt")
+
+## Add names to column names, and add activities and subjects to data.frames
+## in both train and test
+
+colnames(train) <- features[,2]
+colnames(test) <- features[,2]
+colnames(activities) <- c("key", "activity")
+colnames(label_test) <- "label"
+colnames(label_train) <- "label"
+colnames(subject_test) <- "subject"
+colnames(subject_train) <- "subject"
+
+library(dplyr)
+activity_test <- merge(label_test, activities, by.x = "label", by.y = "key")
+activity_train <- merge(label_train, activities, by.x = "label", by.y = "key")
+activity_test <- select(activity_test, activity)
+activity_train <- select(activity_train, activity)
+
+train <-cbind(subject_train,activity_train, train)
+test <-cbind(subject_test,activity_test, test)
+
+
 ## Code assumes data has been downloaded, merged with subject and activity, 
 ## and saved into two files, test and train.
 
